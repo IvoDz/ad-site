01408dd359d1f62c5e6e591abd70a3da9ec20f14
IvoDz <ivo.dz2003@gmail.com>
2023-06-17 16:34:12 +0300

    Add index method to Category controller

--- adsite/app/Http/Controllers/CategoryController.php
+++ adsite/app/Http/Controllers/CategoryController.php
@@ -1,10 +1,17 @@
 <?php
 
 namespace App\Http\Controllers;
+use App\Models\Advertisement;
+use App\Models\Category;
 
 use Illuminate\Http\Request;
 
 class CategoryController extends Controller
 {
-    //
+    public function index()
+{
+    $advertisements = Advertisement::all();
+    $categories = Category::all();
+    return view('home', ['advertisements' => $advertisements, 'categories' => $categories]);
+}
 }
