800a19d2dccdfd7660b324a6c891b8df254afccf
IvoDz <ivo.dz2003@gmail.com>
2023-06-17 14:54:48 +0300

    Create index and show views for ads

---
+++ adsite/resources/views/advertisements/index.blade.php
@@ -0,0 +1,10 @@
+<!-- Display all advertisements -->
+<h1> All advertisements </h1>
+@foreach ($advertisements as $advertisement)
+    <div>
+        <h2><a href="{{ route('advertisements.show', $advertisement->id) }}">{{ $advertisement->title }}</a></h2>
+        <p>Price: ${{ $advertisement->price }}</p>
+        <!-- Display other advertisement details -->
+        <hr>
+    </div>
+@endforeach
---
+++ adsite/resources/views/advertisements/show.blade.php
@@ -0,0 +1,7 @@
+<!-- Display advertisement details -->
+<h2>{{ $advertisement->title }}</h2>
+<p>Price: ${{ $advertisement->price }}</p>
+<!-- Display other advertisement details -->
+<p>Description: {{ $advertisement->description }}</p>
+<!-- Display additional details as needed -->
+<p>Seller: {{ $advertisement->seller_id }}</p>
