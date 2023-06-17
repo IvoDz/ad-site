8b744d3a68ae69e807176468142103130eb0f623
IvoDz <ivo.dz2003@gmail.com>
2023-06-17 16:32:10 +0300

    Extended homepage view

--- adsite/resources/views/home.blade.php
+++ adsite/resources/views/home.blade.php
@@ -1,12 +1,12 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Homepage</title>
-</head>
-<body>
-    <h1> Advertisment portal homepage</h1>
-</body>
-</html>
+<h1>Home Page</h1>
+
+<h2>Categories:</h2>
+<ul>
+    @foreach ($categories as $category)
+        <li><a href="{{ route('advertisements.listByCategory', $category->name) }}">{{ $category->name }}</a></li>
+    @endforeach
+</ul>
+
+<p><a href="{{ route('advertisements.index') }}">Browse All Ads</a></p>
+
+<p><a href="{{ route('advertisements.create')}}"> Create New Ad</a></p>
