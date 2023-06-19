b2b444bb956be502911b86864f240d75de5c3b5e
IvoDz <ivo.dz2003@gmail.com>
2023-06-19 13:11:19 +0300

    Install breeze

--- adsite/routes/web.php
+++ adsite/routes/web.php
@@ -1,7 +1,9 @@
 <?php
 
-use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\ProfileController;
 use App\Http\Controllers\AdvertisementController;
+use App\Http\Controllers\CategoryController;
+use Illuminate\Support\Facades\Route;
 
 /*
 |--------------------------------------------------------------------------
@@ -14,12 +16,27 @@ use App\Http\Controllers\AdvertisementController;
 |
 */
 
+
 Route::get('/', function () {
     return view('home');
 });
+Route::get('/', [CategoryController::class, 'index']);
 
 Route::get('/ads', [AdvertisementController::class, 'index']);
 
 Route::get('/ads/category/{category_name}', [AdvertisementController::class, 'listByCategory']);
 
 Route::get('/ads/{id}', [AdvertisementController::class, 'show'])->name('advertisements.show');
+
+Route::get('/dashboard', function () {
+    return view('dashboard');
+})->middleware(['auth', 'verified'])->name('dashboard');
+
+Route::middleware('auth')->group(function () {
+    Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
+    Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
+    Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
+});
+
+require __DIR__.'/auth.php';
+
