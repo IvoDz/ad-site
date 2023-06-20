9f458c7b8e4ef92d0fa1071c87076bcc5d0df8a8
IvoDz <ivo.dz2003@gmail.com>
2023-06-21 01:01:42 +0300

    Minor changes

--- adsite/routes/web.php
+++ adsite/routes/web.php
@@ -1,7 +1,6 @@
 <?php
 
 use App\Http\Controllers\ProfileController;
-use App\Http\Controllers\ProfileController;
 use App\Http\Controllers\AdvertisementController;
 use App\Http\Controllers\CategoryController;
 use Illuminate\Support\Facades\Route;
@@ -18,9 +17,10 @@ use Illuminate\Support\Facades\Route;
 */
 Route::get('/', [CategoryController::class, 'index']);
 
-Route::get('/ads', [AdvertisementController::class, 'index']);
-
-Route::get('/ads/category/{category_name}', [AdvertisementController::class, 'listByCategory']);
+Route::post('/ads', [AdvertisementController::class, 'store'])->name('advertisements.store');
+Route::get('/ads', [AdvertisementController::class, 'index'])->name('advertisements.index');
+Route::get('/ads/category/{category_name}', [AdvertisementController::class, 'listByCategory'])->name('advertisements.listByCategory');
+Route::get('/ads/create', [AdvertisementController::class, 'create'])->name('advertisements.create');
 
 Route::get('/ads/{id}', [AdvertisementController::class, 'show'])->name('advertisements.show');
 
