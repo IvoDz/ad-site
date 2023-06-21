a55fdd04aa1075e9ef932766fd73e75cda63bedd
IvoDz <ivo.dz2003@gmail.com>
2023-06-21 16:59:53 +0300

    Handle picture storing locally

--- adsite/app/Http/Controllers/AdvertisementController.php
+++ adsite/app/Http/Controllers/AdvertisementController.php
@@ -6,6 +6,10 @@ use App\Models\Category;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
 use App\Models\User;
+use App\Models\File;
+use Illuminate\Validation\Rule;
+use Intervention\Image\Facades\Image;
+use Illuminate\Support\Facades\Storage;
 
 class AdvertisementController extends Controller
     {
@@ -37,23 +41,37 @@ class AdvertisementController extends Controller
     {
         $validatedData = $request->validate([
             'title' => 'required',
-            'price' => 'required|numeric',
-            'category_id' => 'required|exists:categories,id',
+            'price' => 'required',
+            'category_id' => 'required',
             'description' => 'required',
+            'picture' => [
+                'image',
+                'max:100000', // Maximum file size of 2MB (2048 kilobytes)
+            ],
         ]);
 
+        $file = new File();
+        $file->path = $request->file('picture')->store('public/images');
+        $file->type = $request->file('picture')->getClientOriginalExtension();
+        $file->save();
+
         $advertisement = Advertisement::create([
-            'title' => $validatedData['title'],
-            'price' => $validatedData['price'],
-            'category_id' => $validatedData['category_id'],
-            'description' => $validatedData['description'],
-            'seller_id' => Auth::id(),
-        ]);
+                    'title' => $validatedData['title'],
+                    'price' => $validatedData['price'],
+                    'category_id' => $validatedData['category_id'],
+                    'description' => $validatedData['description'],
+                    'seller_id' => Auth::id(),
+                    'pic' => $file->id,
+                ]);
+        // Resize and store the image
+        $imagePath = storage_path('app/' . $file->path);
+        $resizedImagePath = 'public/images/' . $file->id . '.' . $file->type;
+        $image = Image::make($imagePath);
+        $image->save(storage_path('app/' . $resizedImagePath));
 
         $user = User::find(Auth::id());
         $user->increment('amountlisted');
         $user->refresh();
-        // Perform any additional actions or redirects as needed
 
         return redirect()->route('advertisements.show', $advertisement->id);
     }
