532730912a3ce01a97c26f59028f880937281246
IvoDz <ivo.dz2003@gmail.com>
2023-06-21 16:58:09 +0300

    Add 'pic' of Advertisement to $fillable

--- adsite/app/Models/Advertisement.php
+++ adsite/app/Models/Advertisement.php
@@ -4,13 +4,19 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use App\Models\File;
 
 class Advertisement extends Model
 {
-    protected $fillable = ['title', 'price', 'category_id', 'description', 'seller_id'];
+    protected $fillable = ['title', 'price', 'category_id', 'description', 'seller_id', 'pic'];
 
     public function seller()
 {
     return $this->belongsTo(User::class, 'seller_id');
 }
+   public function file()
+{
+    return $this->belongsTo(File::class, 'pic');
+}
+
 }
